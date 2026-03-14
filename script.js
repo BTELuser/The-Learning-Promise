bb4c98c068309055a765cebc547d14ee37c688f8
BTELuser <d4417807@gmail.com>
2026-03-14 14:56:53 +0000

    Add navigation and console log to script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<nav>
+<a href="index.html">Home</a>
+<a href="about.html">About</a>
+<a href="problem.html">The Problem</a>
+<a href="solutions.html">Solutions</a>
+<a href="action.html">Take Action</a>
+</nav>
+console.log("Education awareness site loaded.");
